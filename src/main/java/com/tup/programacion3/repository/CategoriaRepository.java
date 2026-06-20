06257cb76943d05b5fd3dfcd035d4895e80ff624
Lucanms <lucanms@proton.me>
2026-06-20 19:38:39 -0300

    feat: agregar CategoriaRepository extendiendo BaseRepository

--- src/main/java/com/tup/programacion3/repository/CategoriaRepository.java
+++ src/main/java/com/tup/programacion3/repository/CategoriaRepository.java
@@ -1,4 +1,12 @@
 package com.tup.programacion3.repository;
+import com.tup.programacion3.entities.Categoria;
 
-public class CategoriaRepository {
+public class CategoriaRepository extends BaseRepository<Categoria> {
+
+    /**
+     * Constructor que pasa Categoria.class al repositorio base.
+     */
+    public CategoriaRepository() {
+        super(Categoria.class);
+    }
 }
